ddb6930f471048e22602499876c0fd656e985a63
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2025-03-16 00:20:22 +0400

    First JS Homework :)

---
+++ Homework/Homework 1/script.js
@@ -0,0 +1,57 @@
+// task 1
+
+let counter = 10
+counter ++
+alert(counter)
+
+// task 2
+
+let num = 5
+alert(num)
+
+num = 10
+alert(num)
+
+// task 3
+
+let sum = 10 + 20 + 30
+alert(sum)
+
+// დავალების პირობაში ბ იყო ცვალდის სახელი მაგრამ შევცვალე იმიტომ რომ ქვევითაც მიწევდა ბ სახელი
+
+// task 4
+
+let a = 100
+let b = 40
+let c = a - b
+let d = 20
+let result = c + d
+alert(result)
+
+// task 5
+
+let a1 = (2 + 3) * 2 + 3
+alert(a1)
+// კოდის გაშვების გარეშე განსაზღვრება როგორ უნდა აქ დავწეროთ?
+// 13 გამოვა :)'
+
+// task 6
+
+let a2 = 13
+let b2= 5
+let c2 = a2 % b2
+alert(c2)
+
+// task 7
+
+let a3 = 3 * 2 ** 3 * 3
+alert(a3)
+
+// ეკრანზე გამოვა 2 ** 3 = 8
+// 3 * 8 = 24
+// 24 * 3 = 72
+
+// task 8
+
+const birthDate = "15.07.2009"
+console.log (birthDate)
