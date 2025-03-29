75baa148e6b4e632018bcfd5a1239534eb8854be
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2025-03-29 19:56:15 +0400

    Fixed Code

--- Classwork/Classwrok 2/script.js
+++ Classwork/Classwrok 2/script.js
@@ -4,13 +4,12 @@ let guess = Number(prompt('Whats your guess'))
 
 
 while (num != guess) {
-    guess
      if (num > guess) {
         alert('Too low')
-     } else if (num < guess) {
+     } else  {
         alert('Too high')
-     } else {
-        alert('You Won')
-     }
-
+     }     
+     guess = Number(prompt('Whats your guess'))
 }
+
+alert('You won')
